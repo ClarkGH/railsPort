ca3f4e81aec3465c5fdc9656d28303c3052cb28a
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-07-19 16:50:28 -0700

    Few views and file trimmings -removed models for now

--- portfolio/app/models/project.rb
+++
@@ -1,5 +0,0 @@
-class Project < ActiveRecord::Base
-    belongs_to :item, :validate => true
-    attr_accessible :item, :authors, :collaborators, :year, :item_attributes
-    accepts_nested_attributes_for :item
-end
